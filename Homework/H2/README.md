6cb02288cdb592475f66db2debeae3cb06ecea32
unknown <songchaow@outlook.com>
2017-09-10 11:16:09 +0800

    change designs according to the QQ dashboard

--- Homework/H2/README.md
+++ Homework/H2/README.md
@@ -6,13 +6,21 @@
 >
 > 作业原要求为：手工编写能识别输入串中多个满足该NFA的记号的词法分析程序。
 
-> 要求未明确写出，多个记号是由空格分隔还是怎样。
+> 要求未明确指出，多个记号是由空格分隔还是怎样。
 >
 > 我目前把要求理解并实现为：
 >
 > ​        找出输入字符串中所有匹配给定正则表达式的子串，功能类似python的`re.search()`。
 >
 > ​        匹配的子串尽可能达到最大长度。
+
+9.10 更新：
+
+​    根据QQ公告修改：
+
+- 程序原则上仅匹配一个正则表达式(即给定的aa*bb*)，且从开头匹配，在遇到非法字符后会报错，而不是跳过、重新开始匹配。
+- 程序会parse出所有的词法单元(即被匹配的字符串中，对应的aa\*和bb\*两部分)。
+- **程序具有一定的容错能力**：在aa\*前、aa\*与bb\*之间、bb\*出现的非法字符会被忽略。
 
 ## Buildings
 
@@ -28,17 +36,28 @@ gcc main.c
 
 ## Usage
 
-构建完成后，执行输出程序。输入字符串，则可以输出正则表达式`aa*|bb*`的匹配结果。
+构建完成后，执行输出程序。输入字符串，则可以输出正则表达式`aa*|bb*`在该字符串中的匹配结果。
 
 输入`aaab`，匹配到如下结果：
 
-```
+```powershell
 Input a string:
 aaab
-match the re at position 0, with length 3:aaa
-match the re at position 1, with length 2:aa
-match the re at position 2, with length 1:a
-match the re at position 3, with length 1:b
+find lexical unit: aaa
+find lexical unit: b
+请按任意键继续. . .
+```
+
+容错测试：
+
+```powershell
+Input a string:
+aaadddbbb
+find lexical unit: aaa
+Encounter a invalid character, skip...
+Encounter a invalid character, skip...
+Encounter a invalid character, skip...
+find lexical unit: bbb
 请按任意键继续. . .
 ```
 
--- Homework/H2/main.c
+++ Homework/H2/main.c
@@ -4,26 +4,42 @@
     我目前把要求理解并实现为：
         找出输入字符串中所有匹配给定正则表达式的子串，功能类似python的re.search。
         匹配的子串尽可能达到最大长度，即使更小的长度也能达到正规式的要求。
+    9.10 更新：
+    根据公告修改：
+    程序原则上仅匹配一个正则表达式(即给定的aa*bb*)，且从开头匹配，在遇到非法字符后会报错，而不是跳过、重新开始匹配。
+    程序会parse出所有的词法单元(即被匹配的字符串中，对应的aa*和bb*两部分)
 */
 #include <stdio.h>
 int match(char* a)
 // return the length that matched
 {
+    char* p = a;
+    //deal with inrelevant letters
+    while((*a!='a') && (*a!='b') ) {printf("Encounter a invalid character, skip...\n");a++;p++;}
     if(*a=='a')
     {
         int i=1;
         while(*++a=='a')
             i++;
-        return i;
+        printf("find lexical unit: ");
+        for(;p<a;p++)
+            putchar(*p);
+        putchar('\n');
+        
     }
-    else if(*a=='b')
+    while((*a!='a') && (*a!='b') ) {printf("Encounter a invalid character, skip...\n");a++;p++;}
+
+    if(*a=='b')
     {
-        int i=1;
-        while(*++a=='b')
-            i++;
-        return i;
+        while(*++a=='b');
+        printf("find lexical unit: ");
+        for(;p<a;p++)
+            putchar(*p);
+        putchar('\n');
     }
-    else return 0;
+    else
+        printf("Error parsing the string.\n");
+    return 0;
 }
 
 int main()
@@ -32,7 +48,7 @@ int main()
     char a[100];
     printf("Input a string:\n");
     gets(a);
-    char* p,*it;
+    /* char* p,*it;
     for(p=a;*p!=0;p++)
     {
         int len;
@@ -41,7 +57,8 @@ int main()
             for(it=p;it<=p+len-1;it++)
                 putchar(*it);
             putchar('\n');
-    }
+    } */
+    match(a);
     system("pause");
     return 0;
     
